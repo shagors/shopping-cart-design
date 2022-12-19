455a7732f999c1e3d10d2419012486da87085159
shajjad Shagor <shajjadshagor@gmail.com>
2022-12-19 08:13:19 -0800

    login page edit and design it

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductList from './pages/ProductList';
 import Register from './pages/Register';
@@ -7,7 +8,7 @@ import Register from './pages/Register';
 function App() {
   return (
     <div className="">
-      <Register></Register>
+      <Login></Login>
     </div>
   );
 }
