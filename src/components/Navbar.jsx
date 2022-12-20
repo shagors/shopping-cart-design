3fc8c2191e8fa17ebaff09ac1d403b7d497b8e08
shajjad Shagor <shajjadshagor@gmail.com>
2022-12-20 08:39:35 -0800

    design complete and reposvie it and make it comfort any use

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,6 +49,7 @@ const Center = styled.div`
 
 const Logo = styled.h1`
     font-weight: bold;
+    ${mobile({ fontSize: "24px" })}
 `;
 
 const Right = styled.div`
@@ -56,12 +57,14 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+      ${mobile({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({ fontSize: "12px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
@@ -71,7 +74,7 @@ const Navbar = () => {
             <Left>
                 <Language>EN</Language>
                 <SearchContainer>
-                    <Input />
+                    <Input placeholder='search' />
                     <Search style={{color:"gray", fontSize:"16px"}} />
                 </SearchContainer>
             </Left>
